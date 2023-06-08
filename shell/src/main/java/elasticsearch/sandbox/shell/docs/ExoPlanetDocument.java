a90c7c73abd752238311736d6111c88570ac9e22
Nick Mancuso <nicholas.mancuso13@gmail.com>
2023-06-07 23:50:47 -0400

    create pojos from csv

---
+++ shell/src/main/java/elasticsearch/sandbox/shell/docs/ExoPlanetDocument.java
@@ -0,0 +1,57 @@
+package elasticsearch.sandbox.shell.docs;
+
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExoPlanetDocument {
+    @JsonProperty("pl_name")
+    private String planetName;
+
+    @JsonProperty("hostname")
+    private String hostName;
+
+    @JsonProperty("pl_letter")
+    private String planetLetter;
+
+    @JsonProperty("gaia_id")
+    private String gaiaId;
+
+    @JsonProperty("sy_snum")
+    private int numberOfStars;
+
+    @JsonProperty("sy_pnum")
+    private int numberOfPlanets;
+
+    @JsonProperty("sy_mnum")
+    private int numberOfMoons;
+
+    @JsonProperty("discoverymethod")
+    private String discoveryMethod;
+
+    @JsonProperty("disc_year")
+    private int discoveryYear;
+
+    @JsonProperty("disc_facility")
+    private String discoveryFacility;
+
+    @JsonProperty("pl_orbper")
+    private double orbitalPeriod;
+
+    @JsonProperty("pl_orbpererr1")
+    private double orbitalPeriodUpperUncertainty;
+
+    @JsonProperty("pl_orbpererr2")
+    private double orbitalPeriodLowerUncertainty;
+
+    @JsonProperty("pl_orbperlim")
+    private int orbitalPeriodLimitFlag;
+
+    @JsonProperty("st_spectype")
+    private String spectralType;
+}
---
+++ shell/src/test/java/elasticsearch/sandbox/shell/ShellApplicationTests.java
@@ -0,0 +1,13 @@
+package elasticsearch.sandbox.shell;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShellApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

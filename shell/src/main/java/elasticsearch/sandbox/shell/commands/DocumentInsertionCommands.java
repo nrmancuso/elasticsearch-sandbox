ce38a3f39e8da9beac7885b275052d57e24b6158
Nick Mancuso <nicholas.mancuso13@gmail.com>
2023-06-08 00:19:05 -0400

    set up tests

--- shell/src/main/java/elasticsearch/sandbox/shell/commands/DocumentInsertionCommands.java
+++ shell/src/main/java/elasticsearch/sandbox/shell/commands/DocumentInsertionCommands.java
@@ -1,20 +1,12 @@
 package elasticsearch.sandbox.shell.commands;
 
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
 
-import com.fasterxml.jackson.databind.MappingIterator;
-import com.fasterxml.jackson.databind.ObjectReader;
-import com.fasterxml.jackson.dataformat.csv.CsvMapper;
-import com.fasterxml.jackson.dataformat.csv.CsvSchema;
 import elasticsearch.sandbox.shell.docs.ExoPlanetDocument;
+import elasticsearch.sandbox.shell.service.DocumentUtils;
 import lombok.extern.log4j.Log4j2;
 
 @ShellComponent
@@ -34,25 +26,8 @@ public class DocumentInsertionCommands {
         log.info("Index: {}", index);
         log.info("Filename: {}", filename);
 
-        CsvMapper csvMapper = new CsvMapper();
-        CsvSchema csvSchema = CsvSchema.emptySchema().withHeader();
-        final ObjectReader objectReader = csvMapper
-                .readerFor(ExoPlanetDocument.class)
-                .with(csvSchema);
-
-        final List<ExoPlanetDocument> exoPlanetDocuments = new ArrayList<>();
-        try (Reader reader = new FileReader(filename, StandardCharsets.UTF_8)) {
-            final MappingIterator<ExoPlanetDocument> mi = objectReader
-                    .readValues(reader);
-            while (mi.hasNext()) {
-                final ExoPlanetDocument current = mi.next();
-                exoPlanetDocuments.add(current);
-                log.info("Current: {}", current);
-            }
-        } catch (IOException e) {
-            log.error("Error reading file: {}", e.getMessage());
-        }
-
+        final List<ExoPlanetDocument> exoPlanetDocuments =
+                DocumentUtils.generateExoPlanetDocuments(filename);
         log.info("ExoPlanetDocuments: {}", exoPlanetDocuments);
     }
 }
